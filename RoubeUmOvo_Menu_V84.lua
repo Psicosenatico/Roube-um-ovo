78acedb8d741a39df9461ff2870bc54062c9ee81
Psicosenatico <dionep580@icloud.com>
2026-09-15 23:32:59 -0300

    Load Inventory ESP V5

--- RoubeUmOvo_Menu_V84.lua
+++ RoubeUmOvo_Menu_V84.lua
@@ -1,5 +1,5 @@
--- PSICOSENATICO | Roube um Ovo - Precision Menu V8.4.3
--- Loads stable V8.3 base and injects independent Inventory Egg ESP V4.
+-- PSICOSENATICO | Roube um Ovo - Precision Menu V8.4.4
+-- Loads stable V8.3 base and injects independent Inventory Egg ESP V5.
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Psicosenatico/Roube-um-ovo/main/RoubeUmOvo_Menu.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Psicosenatico/Roube-um-ovo/main/InventoryEggESP_V4.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Psicosenatico/Roube-um-ovo/main/InventoryEggESP_V5.lua"))()

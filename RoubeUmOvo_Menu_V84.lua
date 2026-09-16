2735cb402925f5b2a6d76acac095edf0dcbf4979
Psicosenatico <dionep580@icloud.com>
2026-09-15 23:35:26 -0300

    Extend inventory ESP to hotbar backpack slots

--- RoubeUmOvo_Menu_V84.lua
+++ RoubeUmOvo_Menu_V84.lua
@@ -1,5 +1,7 @@
--- PSICOSENATICO | Roube um Ovo - Precision Menu V8.4.4
--- Loads stable V8.3 base and injects independent Inventory Egg ESP V5.
+-- PSICOSENATICO | Roube um Ovo - Precision Menu V8.4.5
+-- Loads stable V8.3 base, Inventory ESP V5 for expanded inventory,
+-- and Hotbar Addon V1 for Backpack.Hotbar slots.
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Psicosenatico/Roube-um-ovo/main/RoubeUmOvo_Menu.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Psicosenatico/Roube-um-ovo/main/InventoryEggESP_V5.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Psicosenatico/Roube-um-ovo/main/InventoryHotbarESP_Addon_V1.lua"))()

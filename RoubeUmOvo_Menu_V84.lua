86363496fdddfda0eed0a0ae3f50f93f0ea90ebd
Psicosenatico <dionep580@icloud.com>
2026-09-15 23:13:56 -0300

    Switch inventory ESP loader to V3 independent overlay

--- RoubeUmOvo_Menu_V84.lua
+++ RoubeUmOvo_Menu_V84.lua
@@ -1,5 +1,5 @@
--- PSICOSENATICO | Roube um Ovo - Precision Menu V8.4.1
--- Loads the stable V8.3 base and injects Inventory Egg ESP V2 into the same menu.
+-- PSICOSENATICO | Roube um Ovo - Precision Menu V8.4.2
+-- Loads the stable V8.3 base and injects Inventory Egg ESP V3 into the same menu.
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Psicosenatico/Roube-um-ovo/main/RoubeUmOvo_Menu.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Psicosenatico/Roube-um-ovo/main/InventoryEggESP_V2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Psicosenatico/Roube-um-ovo/main/InventoryEggESP_V3.lua"))()

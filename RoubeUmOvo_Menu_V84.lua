a7ecea3ced3e29a2bb524bc48a9e92b26d2b77b5
Psicosenatico <dionep580@icloud.com>
2026-09-17 01:25:36 -0300

    Cleanup obsolete V8.4 loader

--- RoubeUmOvo_Menu_V84.lua
+++
@@ -1,6 +0,0 @@
--- PSICOSENATICO | Roube um Ovo - Precision Menu V8.6
--- Stable V8.3 base + working internal inventory panel + visual/equip enhancer.
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Psicosenatico/Roube-um-ovo/main/RoubeUmOvo_Menu.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Psicosenatico/Roube-um-ovo/main/InventoryEggPanel_V1.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Psicosenatico/Roube-um-ovo/main/InventoryEggPanel_Enhancer_V1.lua"))()
